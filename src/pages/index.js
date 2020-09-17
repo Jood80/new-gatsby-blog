e7d1591f69c37be30ee8d4c90c5e945cc41d657a
Nujood <shawar.nujood@gmail.com>
2020-09-17 17:55:26 +0300

    pulling data from markdown files at gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,49 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { graphql, Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
 
-const IndexPage = () => (
+export default ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <div>
+      <h1>Nujood's Thoughts</h1>
+      <h4>{data.allMarkdownRemark.totalCount} </h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <span>
+            {node.frontmatter.title} - {node.frontmatter.date}
+          </span>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          html
+          frontmatter {
+            description
+            title
+            date
+          }
+          excerpt
+        }
+      }
+      totalCount
+    }
+  }
+`;
