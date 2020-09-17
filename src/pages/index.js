ac8533aad35772f7b8b2b5f0c3bceece4729e077
Nujood <shawar.nujood@gmail.com>
2020-09-17 21:28:12 +0300

    update style

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,27 @@ import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: #663399;
+`;
 
 export default ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <div>
       <h1>Nujood's Thoughts</h1>
-      <h4>{data.allMarkdownRemark.totalCount} </h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
-          <span>
-            {node.frontmatter.title} - {node.frontmatter.date}
-          </span>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitle>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </BlogTitle>
+          </BlogLink>
           <p>{node.excerpt}</p>
         </div>
       ))}
